34288d260c8203310b2f0353667c939d1562994d
KenjiKawabata <kawabatatbegoat@gmail.com>
2025-02-05 19:50:50 +0900

    fix-week9

--- week9/froshims/app.py
+++ week9/froshims/app.py
@@ -1,29 +1,50 @@
-# セッション
-from flask import Flask, redirect, render_template, request, session
-from flask_session import Session
+# DBに保存して一覧表示
+from cs50 import SQL
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+db = SQL("sqlite:///froshims.db")
+
+REGISTRANTS = {}
+
+SPORTS = [
+    "Basketball",
+    "Soccer",
+    "Ultimate Frisbee"
+]
 
 @app.route("/")
-def index():
-    if not session.get("name"):
-        return redirect("/login")
-    return render_template("index.html")
+def index() :
+    return render_template("index.html", sports=SPORTS)
 
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        session["name"] = request.form.get("name")
-        return redirect("/")
-    return render_template("login.html")
+@app.route("/deregister", methods=["POST"])
+def deregister() :
+    id = request.form.get("id")
+    if id:
+        db.execute("DELETE FROM registrants WHERE id = ?", id)
+    return redirect("/registrants")
 
 
-@app.route("/logout")
-def logout():
-    session["name"] = None # nameを空にする
-    return redirect("/")
+@app.route("/register", methods=["POST"])
+def register():
+
+    name = request.form.get("name")
+    if not name:
+        return render_template("error.html", message="Missing name")
+    sport = request.form.get("sport")
+    if not sport:
+        return render_template("error.html", message="Missing sport")
+    if sport not in SPORTS:
+        return render_template("error.html", message="Invalid sport")
+
+    db.execute("INSERT INTO registrants (name, sport) VALUES(?, ?)", name, sport)
+
+    return redirect("/registrants")
+
+
+@app.route("/registrants")
+def registrants():
+    registrants = db.execute("SELECT * FROM registrants")
+    return render_template("registrants.html", registrants=registrants)
